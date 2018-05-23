25b6c464d1a1b7a818dee37c35bd3767c41d3fbc
Sammy Akharaz <ketrixz@gmail.com>
2018-05-23 22:06:02 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class CashRegister
   
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount, :items
   
   def initialize(discount = 0)
     @total = 0
@@ -26,6 +26,10 @@ class CashRegister
   
   def items
     @items
+  end
+  
+  def void_last_transaction
+    items.
   end
 end
 
