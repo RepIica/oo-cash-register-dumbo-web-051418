b86aa9749f1d5bef0dd0e6c3a382d4cb15298178
Sammy Akharaz <ketrixz@gmail.com>
2018-05-23 23:07:12 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,9 +12,8 @@ class CashRegister
 
   def add_item(title, price, quantity = 1)
     self.total += price * quantity
-    quantity.times do
-      items[title] = price
-    end
+    quantity.times {items << title}
+    last_transaction_total = price * quantity
   end
   
   def apply_discount
@@ -32,4 +31,4 @@ class CashRegister
   end
 end
 
-binding.pry
+# binding.pry
